21756964aeb998232bfcb071fae554ff02fda13b
settyan <senaredline@gmail.com>
2021-07-17 00:37:26 +0900

    refactor: remove unused imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "normalize.css";
 import "@/styles/globals.css";
 import { AppProps } from "next/dist/next-server/lib/router/router";
-import { getLayout } from "@/layouts/default-layout";
 
 const MyApp = (props: AppProps): JSX.Element => {
   // @ts-ignore
